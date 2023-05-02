bfdd350fcf533ff680e3928c5566a9d6e753354d
Jamie <jamie.danielle@denverrollerderby.org>
2023-05-02 12:12:16 -0600

    wave 4 completed and planets added to database

--- app/routes.py
+++ app/routes.py
@@ -36,21 +36,52 @@ def get_all_planets():
         )
     return jsonify(results)
 
+def validate_planet(id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response({"message": f"planet {id} is invalid"}, 400))
 
-# class Planet:
-#     def __init__(self, id, name, description, distance_from_sun):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.distance_from_sun = distance_from_sun
+    planet = Planet.query.get(id)
+    
+    if not planet:
+        message = f"planet {id} not found"
+        abort(make_response({"message": message}, 404))
+        
+    return planet 
+    
+@planet_bp.route("/<id>", methods=["GET"])
+def get_one_planet(id):
+    planet = validate_planet(id)
+    planet_dict = dict(
+                id=planet.id,
+                name=planet.name,
+                description=planet.description,
+                distance_from_sun=planet.distance_from_sun
+            )
 
-#     def to_dict(self):
-#         return dict(
-#             id=self.id,
-#             name=self.name,
-#             description=self.description,
-#             distance_from_sun=self.distance_from_sun
-#         )
+    return planet_dict
+
+@planet_bp.route("/<id>", methods = ["PUT"])
+def update_one_planet(id):
+    planet_data = request.get_json()
+    planet_to_update = validate_planet(id)
+    
+    planet_to_update.name = planet_data["name"]
+    planet_to_update.description = planet_data["description"]
+    planet_to_update.distance_from_sun = planet_data["distance_from_sun"]
+    db.session.commit()
+    
+    return make_response(f"Planet {planet_to_update.name} updated", 200)
+
+@planet_bp.route("/<id>", methods = ["DELETE"])
+def delete_one_planet(id):
+    planet_to_delete = validate_planet(id)
+    db.session.delete(planet_to_delete)
+    db.session.commit()
+    
+    return make_response(f"Planet {planet_to_delete.name} deleted", 200)
+
 
 
 # planets = [Planet(1, "Mercury", "Mercury has zero moons", 3.5),
@@ -63,33 +94,3 @@ def get_all_planets():
 #            Planet(8, "Neptune", "Neptune has fourteen moons", 288),
 #            Planet(9, "Pluto", "Pluto has five moons", 367)
 #            ]
-
-
-
-# @planet_bp.route("", methods=["GET"])
-# def get_all_planets():
-#     all_planets_response = []
-#     for planet in planets:
-#         all_planets_response.append(planet.to_dict())
-
-#     return jsonify(all_planets_response)
-
-
-# @planet_bp.route("/<id>", methods=["GET"])
-# def get_one_planet(id):
-#     planet = validate_planet(id)
-
-#     return planet.to_dict()
-
-
-# def validate_planet(id):
-#     try:
-#         id = int(id)
-#     except:
-#         abort(make_response({"message": f"planet {id} is invalid"}, 400))
-
-#     for planet in planets:
-#         if planet.id == id:
-#             return planet
-
-#     abort(make_response({"message": f"planet {id} not found"}, 404))

f449db2a5ec6dc10f34313e5a1425e00b2023b6d
lulu <lulusun1210@gmail.com>
2023-05-04 16:47:26 -0500

    change some format to be consistent

--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,45 @@
-from flask import Blueprint, jsonify, abort, make_response,request
+from flask import Blueprint, jsonify, abort, make_response, request
 from app.models.planet import Planet
 from app import db
 
 planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+# POST /planets
+
 
 @planet_bp.route("", methods=["POST"])
 def create_planet():
     request_body = request.get_json()
     try:
         new_planet = Planet.from_dict(request_body)
-    
         db.session.add(new_planet)
         db.session.commit()
-    
+
         message = jsonify(f"Planet {new_planet.name} successfully created")
         return make_response(message, 201)
 
     except KeyError as e:
-        abort(make_response(jsonify(f"missing required value: {e}"), 400))
+        message = jsonify(f"missing required value: {e}")
+        abort(make_response(message, 400))
+
+# GET /planets
+
 
 @planet_bp.route("", methods=["GET"])
 def get_all_planets():
-    planets = Planet.query.all()
-    results = []
-    for planet in planets:
-        results.append(planet.to_dict())
+    name_query = request.args.get("name")
+
+    if name_query:
+        planets = Planet.query.filter_by(name=name_query)
+    else:
+        planets = Planet.query.all()
+
+    results = [planet.to_dict() for planet in planets]
+
     return jsonify(results)
+
+
+# GET /planets/<id>
 
 def validate_model(cls, model_id):
     try:
@@ -35,13 +49,14 @@ def validate_model(cls, model_id):
         abort(make_response(message, 400))
 
     model = cls.query.get(model_id)
-    
+
     if not model:
         message = jsonify(f"{cls.__name__} {model_id} not found")
         abort(make_response(message, 404))
-        
-    return model 
-    
+
+    return model
+
+
 @planet_bp.route("/<id>", methods=["GET"])
 def get_one_planet(id):
     planet = validate_model(Planet, id)
@@ -49,24 +64,30 @@ def get_one_planet(id):
 
     return planet_dict
 
-@planet_bp.route("/<id>", methods = ["PUT"])
+
+# PUT /planets/<id>
+
+@planet_bp.route("/<id>", methods=["PUT"])
 def update_one_planet(id):
     planet_data = request.get_json()
     planet_to_update = validate_model(Planet, id)
-    
+
     planet_to_update.name = planet_data["name"]
     planet_to_update.description = planet_data["description"]
     planet_to_update.distance_from_sun = planet_data["distance_from_sun"]
     db.session.commit()
-    
-    return make_response(jsonify(f"Planet {planet_to_update.name} updated"), 200)
 
-@planet_bp.route("/<id>", methods = ["DELETE"])
+    message = jsonify(f"Planet {planet_to_update.name} updated")
+    return make_response(message, 200)
+
+
+# DELETE /planets/<id>
+
+@planet_bp.route("/<id>", methods=["DELETE"])
 def delete_one_planet(id):
     planet_to_delete = validate_model(Planet, id)
     db.session.delete(planet_to_delete)
     db.session.commit()
-    
-    return make_response(jsonify(f"Planet {planet_to_delete.name} deleted"), 200)
 
-
+    message = jsonify(f"Planet {planet_to_delete.name} deleted")
+    return make_response(message, 200)
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from app import create_app, db
 from flask.signals import request_finished
 from app.models.planet import Planet
 
+
 @pytest.fixture
 def app():
     app = create_app({"TESTING": True})
@@ -18,23 +19,24 @@ def app():
     with app.app_context():
         db.drop_all()
 
+
 @pytest.fixture
 def client(app):
     return app.test_client()
 
+
 @pytest.fixture
 def two_planets(app):
-    Jupiter =  Planet(
+    Jupiter = Planet(
         name="Jupiter",
         description="Jupiter has sixty-seven moons",
-        distance_from_sun= 48.4
+        distance_from_sun=48.4
     )
     Mercury = Planet(
-        name = "Mercury",
-        description = "Mercury has zero moons",
-        distance_from_sun = 3.5
+        name="Mercury",
+        description="Mercury has zero moons",
+        distance_from_sun=3.5
     )
     db.session.add(Jupiter)
     db.session.add(Mercury)
     db.session.commit()
-    
